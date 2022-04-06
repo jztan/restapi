d2d253b3d818cefdf2054e7258f8cddedef84aeb
jztan <jingzheng.tan@gmail.com>
2022-04-06 14:42:46 +0800

    - hide browsable api - add prefix /api/v1/ for rest api

--- restapiproject/urls.py
+++ restapiproject/urls.py
@@ -23,8 +23,8 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('api/v1/', include(router.urls)),
     path('admin/', admin.site.urls),
-    path('api-auth/',
-         include('rest_framework.urls', namespace='rest_framework'))
+    #path('api-auth/',
+    #     include('rest_framework.urls', namespace='rest_framework'))
 ]
