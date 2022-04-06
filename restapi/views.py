1b76053d8a788dec9581677c8293d8e50153ed67
jztan <jingzheng.tan@gmail.com>
2022-04-06 14:43:26 +0800

    - resolved permission issue

--- restapi/views.py
+++ restapi/views.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
 from rest_framework import permissions
+from rest_framework.decorators import permission_classes
+from rest_framework_api_key.permissions import HasAPIKey
 from restapi.serializers import UserSerializer, GroupSerializer
 
 
+@permission_classes([HasAPIKey])
 class UserViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows users to be viewed or edited.
@@ -13,6 +16,7 @@ class UserViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated]
 
 
+@permission_classes([HasAPIKey])
 class GroupViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows groups to be viewed or edited.
