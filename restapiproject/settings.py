54581bfaafb6765e60f29ae19c12972d16609ac2
jztan <jingzheng.tan@gmail.com>
2022-04-07 10:41:00 +0800

    - configure pagination

--- restapiproject/settings.py
+++ restapiproject/settings.py
@@ -128,5 +128,8 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework_api_key.permissions.HasAPIKey',
-    ]
+    ],
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2
 }
